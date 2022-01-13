c1e92d32dd20571a86edf5abd82556faa0fffd06
R-C-McDermott <ryan.c.mcdermott4@gmail.com>
2022-01-13 16:27:18 +0000

    Addition of simple HTML template for displaying news story titles as hyperlinks in the front-end. Addition of webpage.py using flask as the back-end

---
+++ webpage.py
@@ -0,0 +1,36 @@
+"""
+Web-scraping project to extract science news from well known reliable websites
+By Ryan C. McDermott
+
+so far:
+- Able to scrape title and link for article from single website 'Eurekalert'.
+- Develop a webpage for the information to be displayed and accessed (Flask or Django).
+
+to-do:
+- Scrape from more than one website.
+- Construct home page leading to different pages depending on which news website you want.
+- Add abstracts to be displayed under each news story.
+
+"""
+
+from flask import Flask, render_template
+from webscraper import NewsStories
+
+app = Flask(__name__)
+
+
+# Initialise news object from webscraper.py and return latest news
+def initialise_news_object():
+    news_object = NewsStories()
+    news_object.set_date()
+    return news_object.get_news()
+
+
+@app.route("/")
+def home_page():
+    return render_template("index.html", content=news_stories)
+
+
+if __name__ == "__main__":
+    news_stories = initialise_news_object()
+    app.run()
--- webscraper.py
+++ webscraper.py
@@ -1,54 +1,43 @@
-"""
-Web-scraping project to extract science news from well known reliable websites
-
-so far:
-- Able to scrape title and link for article from single website 'Eurekalert'
-
-to-do:
-- Scrape from more than one website
-- Develop a webpage for the information to be displayed and accessed (Flask or Django)
-
-"""
-
 from bs4 import BeautifulSoup
 from datetime import datetime
 import requests
 
 
-# Returns today's date required for latest news in 'Eurek Alert' url
-def get_date():
-    year, month, day = datetime.today().strftime('%Y-%m-%d').split("-")
-    return f"{month}/{day}/{year}"
+class NewsStories:
+    def __init__(self):
+        self.day = None
+        self.month = None
+        self.year = None
 
+    # Returns today's date required for latest news in 'Eurek Alert' url
+    def set_date(self):
+        self.year, self.month, self.day = datetime.today().strftime('%Y-%m-%d').split("-")
 
-# News website url
-base_url = "https://www.eurekalert.org"
-url_eurek_alert = f"https://www.eurekalert.org/news-releases/browse?view=titles&date={get_date()}"
+    def get_news(self):
+        # News website base url and latest news url
+        base_url = "https://www.eurekalert.org"
+        url_eurek_alert = f"https://www.eurekalert.org/news-releases/" \
+                          f"browse?view=titles&date={self.month}/{self.day}/{self.year}"
 
+        r = requests.get(url_eurek_alert)
+        html = r.content
+        bs = BeautifulSoup(html, 'html.parser')
 
-def main():
-    r = requests.get(url_eurek_alert)
-    html = r.content
-    bs = BeautifulSoup(html, 'html.parser')
+        # Find all article sections corresponding to individual stories
+        articles = bs.find_all('article', class_='post')
+        titles, links = [], []
 
-    # Find all article sections corresponding to individual stories
-    articles = bs.find_all('article', class_='post')
-    titles, links = [], []
+        # Extract title and link for each article section in the html code
+        for article in articles:
+            title_tags = article.find_all('h2', class_='post_title')
+            link_tags = article.find_all('a', href=True)
 
-    # Extract title and link for each article section in the html code
-    for article in articles:
-        title_tags = article.find_all('h2', class_='post_title')
-        link_tags = article.find_all('a', href=True)
+            titles.append(title_tags)
+            links.append([(base_url + link.get('href')) for link in link_tags])
 
-        titles.append(title_tags)
-        links.append([(base_url + link.get('href')) for link in link_tags])
+        reform_titles = [title[0].get_text() for title in titles]
+        reform_links = [link[0] for link in links]
 
-    reform_titles = [title[0].get_text() for title in titles]
-    reform_links = [link[0] for link in links]
-
-    # Create dictionary linking titles to links
-    news_stories = dict(zip(reform_titles, reform_links))
-    print(news_stories)
-
-if __name__ == "__main__":
-    main()
+        # Create dictionary linking titles to links
+        news_stories = dict(zip(reform_titles, reform_links))
+        return news_stories
